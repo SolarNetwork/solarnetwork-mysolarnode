be767ad90a96b7106aea6b981df6d01107285fd7
Matt Magoffin <matt@solarnetwork.net>
2020-08-11 11:04:47 +1200

    Resolve deprecation warning from Spring 5.

--- solarssh/src/main/java/net/solarnetwork/solarssh/web/config/WebMvcConfig.java
+++ solarssh/src/main/java/net/solarnetwork/solarssh/web/config/WebMvcConfig.java
@@ -29,7 +29,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import net.solarnetwork.solarssh.web.SolarSshHttpProxyController;
 
@@ -43,12 +43,12 @@ import net.solarnetwork.solarssh.web.SolarSshHttpProxyController;
 @Configuration
 @ComponentScan(basePackages = { "net.solarnetwork.solarssh.web" })
 @EnableScheduling
-public class WebMvcConfig extends WebMvcConfigurerAdapter {
+public class WebMvcConfig implements WebMvcConfigurer {
 
   @Autowired
   private SolarSshHttpProxyController httpProxyController;
 
-  @Scheduled(fixedDelayString = "${ssh.sessionProxyExpireCleanupJobMs}")
+  @Scheduled(fixedDelayString = "${ssh.sessionProxyExpireCleanupJobMs:60000}")
   public void cleanupExpiredSessions() {
     httpProxyController.cleanupExpiredSessions();
   }
