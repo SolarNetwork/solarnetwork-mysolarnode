6361733d8c9b765089dccac85a310c92861f057c
Matt Magoffin <matt@solarnetwork.net>
2021-07-10 08:56:58 +1200

    Fix typo in exception message.

--- solarssh/src/main/java/net/solarnetwork/solarssh/impl/SolarSshPasswordAuthenticator.java
+++ solarssh/src/main/java/net/solarnetwork/solarssh/impl/SolarSshPasswordAuthenticator.java
@@ -196,7 +196,7 @@ public class SolarSshPasswordAuthenticator implements PasswordAuthenticator {
           }
         }
         throw new IOException("Timeout waiting " + this.maxNodeInstructionWaitSecs
-            + "s for session " + sessionId + " node " + nodeId + " connection after instruction {}"
+            + "s for session " + sessionId + " node " + nodeId + " connection after instruction "
             + instructionId + " completed.");
       } else if (state == SolarNodeInstructionState.Declined) {
         log.info("Token {} {} instruction {} was declined.", tokenId, topic, instructionId);
